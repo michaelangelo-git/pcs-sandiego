13ef893652048e043ca767752ff9db2a79e451f6
michaelangelo-git <michaellostrittojr@gmail.com>
2025-12-18 14:39:38 -0500

    addedwebpage

--- dist/pcs-sandiego/server/angular-app-engine-manifest.mjs
+++ dist/pcs-sandiego/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/',
+  basePath: 'pcs-sandiego',
   supportedLocales: {
   "en-US": ""
 },
--- dist/pcs-sandiego/server/angular-app-manifest.mjs
+++ dist/pcs-sandiego/server/angular-app-manifest.mjs
@@ -2,19 +2,18 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/',
+  baseHref: 'pcs-sandiego',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/"
+    "route": "/pcs-sandiego"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6804, hash: 'c56fb360dda2155e038b2d1f835e20bdd5caaa033fc16e687cd3b0fe5d8350c2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 6996, hash: '01410d7bc7bcac6084293b31dd662548788f427a16499d32bf43e9d1604dee0d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 15396, hash: 'b373155e81a74676e1067b869c617334e791813b3d57872043d99c23d80f469b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6815, hash: 'c3b4f722238321106938f52571a98ad5c899aa8406a340450feab05f02c670c9', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7007, hash: '773833b98a79239565b9b6c7368c4ffe398d8809963a8f9fe25214161b15a33f', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-UDREXFSY.css': {size: 196, hash: 'fr51TdA0jJc', text: () => import('./assets-chunks/styles-UDREXFSY_css.mjs').then(m => m.default)}
   },
 };
