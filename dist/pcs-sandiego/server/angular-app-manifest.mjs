a28c94b96059429f6eeede5612c71305fdc8a7ae
Michael Lostritto <michaellostrittojr@gmail.com>
2025-12-18 22:55:44 -0500

    better mobile design

--- dist/pcs-sandiego/server/angular-app-manifest.mjs
+++ dist/pcs-sandiego/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 6817, hash: 'bca77bbdf9b98af7615af905eb242f09654215a9d5f9c88e83b81362d12c05c2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 7009, hash: 'e8c0e563d788df874152d2e934d852e272469772d956eae86845e3423fb0fdf0', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 25858, hash: '259f9c7e718a0f46848268668c8784abc8dff3ee80bb138fe808c1a270fb7ef9', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 6817, hash: 'ba05131bdd27bd4c4e58afa1180bde9c3b7377f4a1e2d949f1fbf3a68d7404db', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 7009, hash: '827450921128ecf6f6e4f45e734f29886c025915dcef2f4e3ad6beeffd62947c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 26800, hash: '2e8c69da2b068713bf47752de635f9368d12e3936cdfdb522a35dc9cfc2e778b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-KID7PUON.css': {size: 8817, hash: 'IMTLOqfXUOc', text: () => import('./assets-chunks/styles-KID7PUON_css.mjs').then(m => m.default)}
   },
 };
